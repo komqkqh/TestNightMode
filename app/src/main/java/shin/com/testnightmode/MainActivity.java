76f69e398dbaddc88cb4e2e21ac93068309f92b1
AFREECATV\komqkqh <komqkqh@afreecatv.com>
2019-10-17 17:02:29 +0900

    add DeviceId

--- app/src/main/java/shin/com/testnightmode/MainActivity.java
+++ app/src/main/java/shin/com/testnightmode/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_theme;
+    TextView tv_dvice_id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +31,34 @@ public class MainActivity extends AppCompatActivity {
         btn_theme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO){
+                if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 }
             }
         });
+
+        tv_dvice_id = findViewById(R.id.tv_dvice_id);
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            } else {
+                tv_dvice_id.setText("" + telephonyManager.getDeviceId());
+            }
+        } else {
+            tv_dvice_id.setText("" + telephonyManager.getDeviceId());
+        }
+
     }
 
     @Override
